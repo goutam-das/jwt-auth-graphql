9bc020f7a105e943ad4d7f5fa5ab09b6db85496d
Goutam Das <goutamdas676@gmail.com>
2021-03-23 09:49:33 +0530

    Basic server setup

---
+++ server/ormconfig.js
@@ -0,0 +1,26 @@
+const DIR = 'dist';
+
+module.exports = {
+    "type": "postgres",
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "synchronize": false,
+    "logging": true,
+    "entities": [
+        `${DIR}/entities/**.entity.{ts,js}`
+    ],
+    "migrations": [
+        `${DIR}/migration/**.{ts,js}`
+    ],
+    "subscribers": [
+        `${DIR}/subscriber/**.{ts,js}`
+    ],
+    "cli": {
+        "entitiesDir": `${DIR}/entities`,
+        "migrationsDir": `${DIR}/migration`,
+        "subscribersDir": `${DIR}/subscriber`
+    }
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,26 @@
 import "reflect-metadata";
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from "type-graphql";
+import { createConnection } from 'typeorm';
 
 (async () => {
     try {
+        // DB Connection
+        await createConnection();
+        console.log('DB Connected!');
+        // Build GraphQL schema
         const schema = await buildSchema({
             resolvers: [__dirname + "/resolvers/**/*.resolver.{ts,js}"],
+        });
+        // Create ApolloServer
+        const apolloServer = new ApolloServer({
+            schema
+        });
+        const app = express();
+        apolloServer.applyMiddleware({ app });
+        app.listen(process.env.PORT,() => {
+            console.log(`Server listen on ${process.env.PORT}`)
         });
     } catch (error) {
         console.error(error)
